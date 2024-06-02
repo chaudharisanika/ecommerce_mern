42683a520960f05aaa9e420ab83c61a7213bcb3b
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2024-06-02 12:22:21 +0530

    middleware done

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from 'morgan';
-import { connect } from "mongoose";
 import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
 
 //configure env
 dotenv.config();
@@ -16,6 +16,9 @@ const app = express()
 //middleware
 app.use(express.json())
 app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth',authRoutes);
 
 //rest api 
 app.get('/',(req,res) => {
