bdb4589357a1010a145ae75e0c2f30bdc41520e4
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2024-07-07 23:44:47 +0530

    done

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/authRoute.js';
 import CategoryRoutes from "./routes/CategoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
+import path from 'path';
 
 //configure env
 dotenv.config();
@@ -31,6 +32,12 @@ app.get('/',(req,res) => {
     res.send('<h1>Welcome</h1>')
 })
 
+//static files
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 //PORT
 const PORT = process.env.PORT || 8080;
 
